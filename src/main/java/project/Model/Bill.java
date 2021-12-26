e73bc55a55026b10f8e5479420e80951fd532bde
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2021-12-26 22:31:40 +0530

    added reminder logic on Bill.java updated user.jsp

--- src/main/java/project/Model/Bill.java
+++ src/main/java/project/Model/Bill.java
@@ -15,14 +15,78 @@ public class Bill {
     private Date date;
     private String boardT;
     private int amount;
+    private int reminder;
 
     @OneToOne(fetch = FetchType.EAGER, cascade=CascadeType.PERSIST)
     @JoinColumn(name = "user_id")
     private User user;
 
     @PrePersist
-    void onCreate(){
+    void onCreate()
+    {
         date = new Date();
+        int number_Of_DaysInMonth = 0;
+        int month=date.getMonth()+1;
+        int year=date.getYear()+1900;
+        switch (month) {
+            case 1:
+                number_Of_DaysInMonth = 31;
+                break;
+            case 2:
+                if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+                    number_Of_DaysInMonth = 29;
+                } else {
+                    number_Of_DaysInMonth = 28;
+                }
+                break;
+            case 3:
+                number_Of_DaysInMonth = 31;
+                break;
+            case 4:
+                number_Of_DaysInMonth = 30;
+                break;
+            case 5:
+                number_Of_DaysInMonth = 31;
+                break;
+            case 6:
+                number_Of_DaysInMonth = 30;
+                break;
+            case 7:
+                number_Of_DaysInMonth = 31;
+                break;
+            case 8:
+                number_Of_DaysInMonth = 31;
+                break;
+            case 9:
+                number_Of_DaysInMonth = 30;
+                break;
+            case 10:
+                number_Of_DaysInMonth = 31;
+                break;
+            case 11:
+                number_Of_DaysInMonth = 30;
+                break;
+            case 12:
+                number_Of_DaysInMonth = 31;
+        }
+
+        //handling next days
+        if(date.getDate()+20>number_Of_DaysInMonth){
+            //setting reminder based on number of days
+            reminder = (date.getDate()+20)%number_Of_DaysInMonth;
+            if(reminder==0) reminder=number_Of_DaysInMonth;
+        }
+        else{
+            reminder = date.getDay()+20;
+        }
+    }
+
+    public int getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(int reminder) {
+        this.reminder = reminder;
     }
 
     public Bill(int dues, int units, String boardT, int amount, User user) {
--- src/main/webapp/user.jsp
+++ src/main/webapp/user.jsp
@@ -7,7 +7,6 @@
 </head>
 <%
     //check reminder of bill
-
     User user = (User)session.getAttribute("current-User");
     if(user==null){
         session.setAttribute("message1","You are not logged in!!");
@@ -24,7 +23,7 @@
             <div class="card">
                 <img class="card-img-top" src="./img/profile.png" alt="View Bill & Profile">
             </div>
-            <a class="link" href="viewInfo.jsp"><br><br><strong>pay.png</strong></a>
+            <a class="link" href="viewInfo.jsp"><br><br><strong>View Profile and Bill</strong></a>
         </div>
         <div class="colindex">
             <div class="card">
